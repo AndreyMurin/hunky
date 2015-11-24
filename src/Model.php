468c6de637d3b9ac73ff865fc4726e457f74cdc0
AndreyMurin <96467840@mail.ru>
2015-11-24 15:49:22 +0300

    tests

---
+++ src/Model.php
@@ -0,0 +1,7 @@
+<?php
+namespace hunky;
+
+class Model
+{
+
+}
---
+++ tests/ControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+class ControllerTest extends PHPUnit_Framework_TestCase
+{
+    // ...
+    public function testCanBeNegated()
+    {
+        // Assert
+        $this->assertEquals(-1, -1);
+    }
+
+    // ...
+}
