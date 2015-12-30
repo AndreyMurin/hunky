302559884bc95bb0f5852071c59bce314285ce61
AndreyMurin <96467840@mail.ru>
2015-12-30 10:26:09 +0300

    1

--- tests/ControllerTest.php
+++ tests/ControllerTest.php
@@ -1,9 +1,15 @@
 <?php
+
+require_once 'hunkey/Controller.php';
+use hunkey;
+
 class ControllerTest extends PHPUnit_Framework_TestCase
 {
     // ...
     public function testCanBeNegated()
     {
+        $controller = new \hunkey\Controller();
+
         // Assert
         $this->assertEquals(-1, -1);
 
